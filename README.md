8e48834d8cd40a724df37f5ca8aa70756490d64a
Maria Camila C <mariacamilacm01@gmail.com>
2026-06-02 14:27:14 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# First implementation of segmentation
+
+The goal of this first development is to use different colour masks and detect cataract fragments during surgery phase.
---
+++ test1_segment.py
@@ -0,0 +1,6 @@
+# This is the first test on segmentation based on our dataset
+# called "pre-dataset"
+
+# Segmentation of cataract fragments using a simple thresholding method
+
+
