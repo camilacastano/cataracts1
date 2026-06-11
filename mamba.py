1819c83a5b629013b79d9cbd090719571eead872
Maria Camila C <mariacamilacm01@gmail.com>
2026-06-10 22:40:40 -0600

    add fragment detection

--- mamba.py
+++ mamba.py
@@ -2,11 +2,26 @@
 
 import os
 import torch
+from videomamba import VideoMamba
+import numpy as np
+from torchvision import transforms
 
-# Load directly via hub configuration or git import 
 # Source: https://github.com/OpenGVLab/VideoMamba
 model_url = "https://huggingface.co/OpenGVLab/VideoMamba/resolve/main/videomamba_b16_in1k_res224.pth"
 checkpoint = torch.hub.load_state_dict_from_url(model_url, map_location="cpu")
 
 print("Official checkpoint loaded successfully.")
+
+"""----------------------"""
+
+video_path="/Users/camilacastano/Desktop/cataracts1/1/Maria Edith Feria Escalera OI .mp4"
+output_path="/Users/camilacastano/Desktop/cataracts1/1/processed_video.mp4"
+
+
+num_frames=16
+img_size=224
+
+devide=torch.device(
+    "cuda" if torch.cuda.is_available() else "cpu"
+)
 
